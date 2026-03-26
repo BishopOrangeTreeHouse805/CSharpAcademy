a2aefba230a4a5c1e482dd0a5b262fc16cca0856
John Paul Naranjo <swade84@hotmail.com>
2026-03-26 08:51:00 -0700

    Added five examples of types

---
+++ Debugger1.0/Debugger1.0/Program.cs
@@ -0,0 +1,75 @@
+﻿
+Console.WriteLine(BetterCardUtilization(0.5, 0.6));
+Console.WriteLine(BetterCardUtilization(0.6, 0.5));
+
+
+BetterCardUtilization(0.5, 0.6);
+BetterCardUtilization(0.6, 0.5);
+
+
+string BetterCardUtilization(double card1Util, double card2Util)
+{
+    if (card1Util == card2Util)
+    {
+        return "Both cards have equal utilization.";
+    }
+
+    bool card1IsBetter = card1Util > card2Util;
+    
+    return card1IsBetter
+        ? "Card 1 is better utilized." 
+        : "Card 2 is better utilized.";
+}
+
+
+// //
+// //
+// // public static class StringHelper
+// // {
+// //     public static string ToTitleCase(this string stringSet)
+// //     {
+// //         if (string.IsNullOrEmpty(stringSet)) //handles null and empty strings
+// //         {
+// //             return stringSet;
+// //         }
+// //         
+// //         char[] stringSetArray = stringSet.ToLower().ToCharArray(); //avoids creating a separate string variable for the first word to attach later.
+// //
+// //         if (char.IsLetter(stringSetArray[0]))//char has special methods for checking if a character is a letter.
+// //         {
+// //             stringSetArray[0] = char.ToUpper(stringSetArray[0]);//char has special methods for uppercasing and lowercasing characters.
+// //         }
+// //
+// //         for (int i = 1; i < stringSetArray.Length; i++)//loops through the char in the array
+// //         {
+// //             if (stringSetArray[i - 1] == ' ' && char.IsLetter(stringSetArray[i]))
+// //             {
+// //                 stringSetArray[i] = char.ToUpper(stringSetArray[i]);
+// //             }
+// //         }
+// //         
+// //         return new string(stringSetArray);
+// //         
+// //     }
+// //
+// //
+// //
+// //
+//
+// //     public static bool IsPalindrome(this string stringSet)
+// //     {
+// //         if (string.IsNullOrEmpty(stringSet))
+// //         {
+// //             return false;
+// //         }
+// //
+// //         char[] stringSetArray = stringSet.ToCharArray();
+// //
+// //         return stringSetArray.Reverse().ToString() == stringSetArray.ToString();//This is wrong.
+// //
+// //     }
+// //     
+// //     
+// // }
+// //
+// //
--- MathGame/MathGame/Program.cs
+++ MathGame/MathGame/Program.cs
@@ -1,3 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("These are the most common data types: ");
+Console.WriteLine(1 + " - string, example: " + "John");
+Console.WriteLine(2 + " - char, example: " + 'j');
+Console.WriteLine(3 + " - int, example: " + 23);
+Console.WriteLine(4 + " - decimal, example: " + 29.90m);
+Console.WriteLine(5 + " - bool, example: " + false);
+Console.ReadLine();
