53f863fa3e08ba64170b087b181dc78abba69fc1
John Paul Naranjo <swade84@hotmail.com>
2026-03-27 11:15:41 -0700

    Implemented the changes made in the tutorial: string interpolation, and the use of variables.

--- MathGame/MathGame/Program.cs
+++ MathGame/MathGame/Program.cs
@@ -1,7 +1,16 @@
-﻿Console.WriteLine("These are the most common data types: ");
-Console.WriteLine(1 + " - string, example: " + "John");
-Console.WriteLine(2 + " - char, example: " + 'j');
-Console.WriteLine(3 + " - int, example: " + 23);
-Console.WriteLine(4 + " - decimal, example: " + 29.90m);
-Console.WriteLine(5 + " - bool, example: " + false);
-Console.ReadLine();
+﻿int index = 1;
+string name = "John";
+char initial = 'J';
+int year = 2026;
+decimal height = 1.78m;
+bool doWeLoveToCode = true;
+
+
+string myParagraph = $@"These are the most common data types: 
+{index++} - string, example: {name}
+{index++} - char, example: {initial}
+{index++} - int, example: {year}
+{index++} - decimal, example: {height}
+{index++} - bool, example: {doWeLoveToCode}";
+
+Console.WriteLine(myParagraph);
